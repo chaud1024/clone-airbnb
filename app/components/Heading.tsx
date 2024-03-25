efb06e53493ef0fe52759dbca5feeab937b5d57a
chaud1024 <chaud1024@gmail.com>
2024-03-25 22:29:47 +0900

    create Heading component

---
+++ app/components/Heading.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+interface HeadingProps {
+  title: string;
+  subTitle?: string;
+  center?: boolean;
+}
+
+const Heading: React.FC<HeadingProps> = ({ title, subTitle, center }) => {
+  return (
+    <div className={center ? "text-center" : "text-start"}>
+      <div className="text-2xl font-bold">{title}</div>
+      <div className="mt-2 font-light text-neutral-500">{subTitle}</div>
+    </div>
+  );
+};
+
+export default Heading;
