e35007b273c6dfb331444f9a5908d58ddd0460c1
chaud1024 <chaud1024@gmail.com>
2024-03-26 17:41:17 +0900

    create getCurrentUser by getServerSession from next-auth/next

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,31 @@
+import prisma from "@/app/libs/prismadb";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+
+export async function getSesseion() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  try {
+    const session = await getSesseion();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma?.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return currentUser;
+  } catch (error: any) {
+    null;
+  }
+}
