86046ebf7e7f95f200c186a057f3c748abdcef3b
chaud1024 <chaud1024@gmail.com>
2024-03-27 14:42:24 +0900

    create constant categories

---
+++ app/constants/cetegories.ts
@@ -0,0 +1,94 @@
+import { BsSnow } from "react-icons/bs";
+import { FaSkiing } from "react-icons/fa";
+import {
+  GiBarn,
+  GiBoatFishing,
+  GiCactus,
+  GiCastle,
+  GiCaveEntrance,
+  GiForestCamp,
+  GiIsland,
+  GiWindmill,
+} from "react-icons/gi";
+import { IoDiamond } from "react-icons/io5";
+import { MdOutlineVilla } from "react-icons/md";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
+import { Category } from "../types/categories";
+
+export const categories = [
+  {
+    label: "Beach",
+    icon: TbBeach,
+    description: "This property is close to the beach!",
+  },
+  {
+    label: "Windmills",
+    icon: GiWindmill,
+    description: "This property has windmills!",
+  },
+  {
+    label: "Modern",
+    icon: MdOutlineVilla,
+    description: "This property is modern!",
+  },
+  {
+    label: "Countryside",
+    icon: TbMountain,
+    description: "This property is in the countryside!",
+  },
+  {
+    label: "Pools",
+    icon: TbPool,
+    description: "This property has a pool!",
+  },
+  {
+    label: "Islands",
+    icon: GiIsland,
+    description: "This property is on an island!",
+  },
+  {
+    label: "Lake",
+    icon: GiBoatFishing,
+    description: "This property is close to a lake!",
+  },
+  {
+    label: "Skiing",
+    icon: FaSkiing,
+    description: "This property has skiing activies!",
+  },
+  {
+    label: "Castles",
+    icon: GiCastle,
+    description: "This property is in a castle!",
+  },
+  {
+    label: "Camping",
+    icon: GiForestCamp,
+    description: "This property has camping activities!",
+  },
+  {
+    label: "Arctic",
+    icon: BsSnow,
+    description: "This property is near arctic!",
+  },
+  {
+    label: "Cave",
+    icon: GiCaveEntrance,
+    description: "This property is in a cave!",
+  },
+  {
+    label: "Desert",
+    icon: GiCactus,
+    description: "This property is in a desert!",
+  },
+  {
+    label: "Barns",
+    icon: GiBarn,
+    description: "This property is in a barn!",
+  },
+  {
+    label: "Lux",
+    icon: IoDiamond,
+    description: "This property is luxurious!",
+  },
+] as Category[];
