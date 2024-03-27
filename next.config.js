71e575efb37de8e4fa1ea11efddc71b9263fce33
chaud1024 <chaud1024@gmail.com>
2024-03-27 10:49:42 +0900

    add image domain config

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+    ],
+  },
+};
