dec429ed1e09638e72ec7bbf45973f1750da18ca
chaud1024 <chaud1024@gmail.com>
2024-03-27 14:42:07 +0900

    add router to home by useRouter from next/navigation

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -7,8 +7,9 @@ const Logo = () => {
   const router = useRouter();
   return (
     <Image
+      onClick={() => router.push("/")}
       alt="logo"
-      className="hidden md:block cursor-pointer"
+      className="hidden cursor-pointer md:block"
       height={100}
       width={100}
       src="/images/logo.svg"
