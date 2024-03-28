d0b835c32889727d54e29cc8bf13f7cec49d914e
chaud1024 <chaud1024@gmail.com>
2024-03-28 18:02:18 +0900

    add EmptyState

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello!</div>;
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols4 xl:grid-cols-5 2xl:grid-cols-6">
+          {listings.map((item: any) => (
+            <div key={item.id}>{item.title}</div>
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
