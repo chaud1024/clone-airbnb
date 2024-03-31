b06d3ac0fcc1c03c3fa3df9479fee9e80ce7d9b2
chaud1024 <chaud1024@gmail.com>
2024-03-31 21:07:13 +0900

    create PropertiesPage

---
+++ app/properties/page.tsx
@@ -0,0 +1,39 @@
+import getCurrentUser from "../actions/getCurrentUser";
+import getListings from "../actions/getListings";
+import ClientOnly from "../components/ClientOnly";
+import EmptyState from "../components/EmptyState";
+import PropertiesClient from "./PropertiesClient";
+
+const PropertiesPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subTitle="Please login" />
+      </ClientOnly>
+    );
+  }
+
+  const listings = await getListings({
+    userId: currentUser.id,
+  });
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No properties found"
+          subTitle="Looks like You have no properties"
+        />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <PropertiesClient listings={listings} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
+
+export default PropertiesPage;
