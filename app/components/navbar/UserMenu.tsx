2418a4f76c7c595a2d0a31b59dff584531152e19
chaud1024 <chaud1024@gmail.com>
2024-03-31 21:07:40 +0900

    add router push to /properties

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -70,7 +70,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   onClick={() => router.push("/reservations")}
                   label="My reservations"
                 />
-                <MenuItem onClick={() => {}} label="My properties" />
+                <MenuItem
+                  onClick={() => router.push("/properties")}
+                  label="My properties"
+                />
                 <MenuItem
                   onClick={() => rentModal.onOpen}
                   label="Airbnb my home"
